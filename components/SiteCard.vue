6fae2b5af133e91e40ba8f7225b72724a06c3a95
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2023-08-19 22:02:38 +0900

    SiteCardのスタイルが一応できた

--- components/SiteCard.vue
+++ components/SiteCard.vue
@@ -35,25 +35,28 @@ a:hover {
   align-items: center;
   width: 40rem;
   max-width: 100%;
-  height: 10rem;
   border: 1px solid var(--vp-c-divider);
 }
 .image {
   /* height: 10rem; */
   height: 100%;
-  width: 15rem;
-  object-fit: cover;
+  max-height: 10rem;
+  /* object-fit: cover; */
 }
 .content {
   max-width: 100%;
   padding: 1rem;
   flex: 1 1 15rem;
-  color: var(--vp-c-text-1);
-  /* display: block; */
+  display: block;
 }
 .title {
   color: var(--vp-c-text-1);
   font-weight: bold;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  -webkit-line-clamp: 2;
+  word-break: break-all;
 }
 .description {
   /* max-height: 10rem; */
